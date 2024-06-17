e89d3d0ceb8364a011bf021f37f0ff095ab12a78
PhwgNamTran <phwgnam@gmail.com>
2024-06-18 00:51:43 +0700

    Add function to handle EXTI line 15_10 Interrupt

---
+++ Project/Interrupt_Handler/Interrupt_Handler.c
@@ -0,0 +1,7 @@
+#include "Interrupt_Handler.h"
+#include "Common.h"
+
+void EXTI15_10_IRQHandler(void)
+{
+    
+}
---
+++ Project/Interrupt_Handler/Interrupt_Handler.h
@@ -0,0 +1,6 @@
+#ifndef INTERRUPT_HANDLER_H
+#define INTERRUPT_HANDLER_H
+
+extern void EXTI15_10_IRQHandler(void);
+
+#endif
