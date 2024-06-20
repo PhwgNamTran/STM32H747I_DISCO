b7cf20f284b3c7a296f209b7c1249fcda13c4c6a
PhwgNamTran <phwgnam@gmail.com>
2024-06-20 22:04:28 +0700

    Fix issue: Missing init periperals call

--- Project/Include/System_Init.h
+++ Project/Include/System_Init.h
@@ -2,5 +2,6 @@
 #define SYSTEM_INIT_H
 
 extern void System_Init(void);
+extern void Periperals_Init(void);
 
 #endif
--- Project/Source/main.c
+++ Project/Source/main.c
@@ -2,10 +2,12 @@
 #include "UserLED.h"
 #include "WkButton.h"
 #include "Project_config.h"
+#include "System_Init.h"
 
 
 int main(void)
 {
+    Periperals_Init();
     while (1) 
     {
 #if(WK_BUTTON_Interrupt != STD_ON)
