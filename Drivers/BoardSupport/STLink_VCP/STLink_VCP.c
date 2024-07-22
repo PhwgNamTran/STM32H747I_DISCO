c1ce6e17b000ae67408eb31ce8194a90dcf02998
PhwgNamTran <phwgnam@gmail.com>
2024-07-22 22:34:03 +0700

    Fix finding: Miss clocksource for GPIO Port of USART Pin

--- Drivers/BoardSupport/STLink_VCP/STLink_VCP.c
+++ Drivers/BoardSupport/STLink_VCP/STLink_VCP.c
@@ -13,6 +13,12 @@
  */
 void STLink_VCP_Init(void)
 {
+    // Enable clock source for USART PIN
+    GPIO_Enable_ClockSource(STLink_VCP_USART_GPIO_Port_CLKSRC);
+
+    // Enable clock source for USART
+    USART_Enable_ClockSource(STLink_VCP_USART);
+
     // Configure USART pins
     USART_PIN_Config(STLink_VCP_USART_GPIO_Port, STLink_VCP_USART_GPIO_AFx,
                      STLink_VCP_USART_Rx_PIN, STLink_VCP_USART_Tx_PIN);
@@ -21,10 +27,7 @@ void STLink_VCP_Init(void)
     USART_Config(STLink_VCP_USART, 16000000U, STLink_VCP_USART_Baudrate, 
                  STLink_VCP_USART_OverSamplingMode, STLink_VCP_USART_DataLengthCode, 
                  STLink_VCP_USART_NumberOfStopBit, STLink_VCP_USART_ParityCheck);
-    
-    // Enable clock source for USART
-    USART_Enable_ClockSource(STLink_VCP_USART);
-    
+
     // Enable USART
     USART_Enable(STLink_VCP_USART);
 }
--- Drivers/BoardSupport/STLink_VCP/STLink_VCP.h
+++ Drivers/BoardSupport/STLink_VCP/STLink_VCP.h
@@ -8,13 +8,14 @@
 
 /* HW configuration for STLink_VCP which connects to USART1 of STM32H747XI on STM32H747I-DISCO */
 #define STLink_VCP_USART_GPIO_Port              GPIO_A                /**< GPIO Port for STLink VCP USART */
+#define STLink_VCP_USART_GPIO_Port_CLKSRC       GPIO_A_CLKSRC         /**< Clocksource for STLink VCP USART Pin */
 #define STLink_VCP_USART_Tx_PIN                 GPIO_PIN_9            /**< GPIO Pin for USART Tx */
 #define STLink_VCP_USART_Rx_PIN                 GPIO_PIN_10           /**< GPIO Pin for USART Rx */
 #define STLink_VCP_USART_GPIO_AFx               GPIO_AF_7             /**< GPIO Alternate Function for USART */
 #define STLink_VCP_USART                        USART1                /**< USART Instance for STLink VCP */
 
 /* Parameter configuration for USART, CAN BE CHANGED */    
-#define STLink_VCP_USART_Baudrate               115200UL                 /**< Baudrate for USART communication */
+#define STLink_VCP_USART_Baudrate               115200UL               /**< Baudrate for USART communication */
 #define STLink_VCP_USART_OverSamplingMode       USART_OverSamplingBy16 /**< Oversampling mode for USART */
 #define STLink_VCP_USART_DataLengthCode         USART_DataBit_8        /**< Data length code for USART */
 #define STLink_VCP_USART_NumberOfStopBit        USART_StopBit_1        /**< Number of stop bits for USART */
