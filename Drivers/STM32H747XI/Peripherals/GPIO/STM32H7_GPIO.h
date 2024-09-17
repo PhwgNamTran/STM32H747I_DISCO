5e4df84ad7828ae11b6e72af18ce3c49db2f276a
PhwgNamTran <phwgnam@gmail.com>
2024-09-17 23:35:52 +0700

    Change subfix for struct to _ST

--- Drivers/STM32H747XI/Peripherals/GPIO/STM32H7_GPIO.h
+++ Drivers/STM32H747XI/Peripherals/GPIO/STM32H7_GPIO.h
@@ -21,22 +21,22 @@ typedef struct
   */
 typedef enum
 {
-    GPIO_PIN_0_N  = 0,  /**< GPIO Pin 0 */
-    GPIO_PIN_1_N,       /**< GPIO Pin 1 */
-    GPIO_PIN_2_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_3_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_4_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_5_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_6_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_7_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_8_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_9_N,       /**< GPIO Pin 2 */
-    GPIO_PIN_10_N,      /**< GPIO Pin 2 */
-    GPIO_PIN_11_N,      /**< GPIO Pin 2 */
-    GPIO_PIN_12_N,      /**< GPIO Pin 2 */
-    GPIO_PIN_13_N,      /**< GPIO Pin 2 */
-    GPIO_PIN_14_N,      /**< GPIO Pin 2 */
-    GPIO_PIN_15_N       /**< GPIO Pin 2 */
+    GPIO_PIN_0_N = 0U,  /*!< GPIO Pin 0 */
+    GPIO_PIN_1_N,       /*!< GPIO Pin 1 */
+    GPIO_PIN_2_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_3_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_4_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_5_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_6_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_7_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_8_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_9_N,       /*!< GPIO Pin 2 */
+    GPIO_PIN_10_N,      /*!< GPIO Pin 2 */
+    GPIO_PIN_11_N,      /*!< GPIO Pin 2 */
+    GPIO_PIN_12_N,      /*!< GPIO Pin 2 */
+    GPIO_PIN_13_N,      /*!< GPIO Pin 2 */
+    GPIO_PIN_14_N,      /*!< GPIO Pin 2 */
+    GPIO_PIN_15_N       /*!< GPIO Pin 2 */
 } GPIO_Pin_N;
 
 /**
@@ -44,10 +44,10 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_Mode_Input_N = 0,              /**< GPIO Input Mode */
-    GPIO_Mode_General_Purpose_Output_N, /**< GPIO General Purpose Output Mode */
-    GPIO_Mode_Alternate_Function_N,     /**< GPIO Alternate Function Mode */
-    GPIO_Mode_Analog_N                  /**< GPIO Analog Mode */
+    GPIO_Mode_Input_N = 0U,             /*!< GPIO Input Mode */
+    GPIO_Mode_General_Purpose_Output_N, /*!< GPIO General Purpose Output Mode */
+    GPIO_Mode_Alternate_Function_N,     /*!< GPIO Alternate Function Mode */
+    GPIO_Mode_Analog_N                  /*!< GPIO Analog Mode */
 } GPIO_Mode_N;
 
 /**
@@ -55,10 +55,10 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_No_Pull_N = 0U,   /**< GPIO No Pull */
-    GPIO_Pull_Up_N,        /**< GPIO Pull Up */
-    GPIO_Pull_Down_N,      /**< GPIO Pull Down */
-    GPIO_Pull_Reserved_N   /**< GPIO Reserved Pull */
+    GPIO_No_Pull_N = 0U,   /*!< GPIO No Pull */
+    GPIO_Pull_Up_N,        /*!< GPIO Pull Up */
+    GPIO_Pull_Down_N,      /*!< GPIO Pull Down */
+    GPIO_Pull_Reserved_N   /*!< GPIO Reserved Pull */
 } GPIO_Pull_N;
 
 /**
@@ -66,10 +66,10 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_Speed_Low_N = 0U,    /**< GPIO Speed Low */
-    GPIO_Speed_Medium_N,      /**< GPIO Speed Medium */
-    GPIO_Speed_High_N,        /**< GPIO Speed High */
-    GPIO_Speed_VeryHigh_N     /**< GPIO Speed Very High */
+    GPIO_Speed_Low_N = 0U,    /*!< GPIO Speed Low */
+    GPIO_Speed_Medium_N,      /*!< GPIO Speed Medium */
+    GPIO_Speed_High_N,        /*!< GPIO Speed High */
+    GPIO_Speed_VeryHigh_N     /*!< GPIO Speed Very High */
 } GPIO_Speed_N;
 
 /**
@@ -77,22 +77,22 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_AF0_N = 0U,  /**< GPIO Alternate Function 0 */
-    GPIO_AF1_N,       /**< GPIO Alternate Function 1 */
-    GPIO_AF2_N,       /**< GPIO Alternate Function 2 */
-    GPIO_AF3_N,       /**< GPIO Alternate Function 3 */
-    GPIO_AF4_N,       /**< GPIO Alternate Function 4 */
-    GPIO_AF5_N,       /**< GPIO Alternate Function 5 */
-    GPIO_AF6_N,       /**< GPIO Alternate Function 6 */
-    GPIO_AF7_N,       /**< GPIO Alternate Function 7 */
-    GPIO_AF8_N,       /**< GPIO Alternate Function 8 */
-    GPIO_AF9_N,       /**< GPIO Alternate Function 9 */
-    GPIO_AF10_N,      /**< GPIO Alternate Function 10 */
-    GPIO_AF11_N,      /**< GPIO Alternate Function 11 */
-    GPIO_AF12_N,      /**< GPIO Alternate Function 12 */
-    GPIO_AF13_N,      /**< GPIO Alternate Function 13 */
-    GPIO_AF14_N,      /**< GPIO Alternate Function 14 */
-    GPIO_AF15_N       /**< GPIO Alternate Function 15 */
+    GPIO_AF0_N = 0U,  /*!< GPIO Alternate Function 0 */
+    GPIO_AF1_N,       /*!< GPIO Alternate Function 1 */
+    GPIO_AF2_N,       /*!< GPIO Alternate Function 2 */
+    GPIO_AF3_N,       /*!< GPIO Alternate Function 3 */
+    GPIO_AF4_N,       /*!< GPIO Alternate Function 4 */
+    GPIO_AF5_N,       /*!< GPIO Alternate Function 5 */
+    GPIO_AF6_N,       /*!< GPIO Alternate Function 6 */
+    GPIO_AF7_N,       /*!< GPIO Alternate Function 7 */
+    GPIO_AF8_N,       /*!< GPIO Alternate Function 8 */
+    GPIO_AF9_N,       /*!< GPIO Alternate Function 9 */
+    GPIO_AF10_N,      /*!< GPIO Alternate Function 10 */
+    GPIO_AF11_N,      /*!< GPIO Alternate Function 11 */
+    GPIO_AF12_N,      /*!< GPIO Alternate Function 12 */
+    GPIO_AF13_N,      /*!< GPIO Alternate Function 13 */
+    GPIO_AF14_N,      /*!< GPIO Alternate Function 14 */
+    GPIO_AF15_N       /*!< GPIO Alternate Function 15 */
 } GPIO_AlternateFunction_N;
 
 /**
@@ -100,8 +100,8 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_Output_PushPull_N = 0U,   /**< GPIO Push-Pull Output Type */
-    GPIO_Output_OpenDrain_N        /**< GPIO Open-Drain Output Type */
+    GPIO_Output_PushPull_N = 0U,   /*!< GPIO Push-Pull Output Type */
+    GPIO_Output_OpenDrain_N        /*!< GPIO Open-Drain Output Type */
 } GPIO_OutputType_N;
 
 /**
@@ -109,8 +109,8 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_LockKey_NotActive_N = 0U,    /**< GPIO Lock Key Not Active */
-    GPIO_LockKey_Active_N             /**< GPIO Lock Key Active */
+    GPIO_LockKey_NotActive_N = 0U,    /*!< GPIO Lock Key Not Active */
+    GPIO_LockKey_Active_N             /*!< GPIO Lock Key Active */
 } GPIO_LockKey_N;
 
 /**
@@ -118,8 +118,8 @@ typedef enum
   */
 typedef enum
 {
-    GPIO_NoLock_N = 0U,    /**< GPIO No Lock */
-    GPIO_Lock_N            /**< GPIO Locked */
+    GPIO_NoLock_N = 0U,    /*!< GPIO No Lock */
+    GPIO_Lock_N            /*!< GPIO Locked */
 } GPIO_Lock_N;
 
 extern void GPIO_Init(GPIO_Handle_ST IO_Pin);
